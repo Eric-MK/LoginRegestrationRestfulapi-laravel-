e8216e2085e64f18f5cbd1d7e6fb148e5ab33b66
unknown <mutungaeric839@gmail.com>
2023-05-11 23:34:30 +0300

    creating register controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('users/{id}', [LoginController::class,'check']);
+
